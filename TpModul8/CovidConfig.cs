5556706fc54b82fefb51122ab4d0c140f7e53c71
kzhoru <adrianputraperkasa16@gmail.com>
2025-04-13 22:34:16 +0700

    menyelesaikan program dengan runtime configuration

---
+++ TpModul8/CovidConfig.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TpModul8
+{
+    class CovidConfig
+    {
+        public string satuan_suhu { get; set; }
+        public int batas_hari_demam { get; set; }
+        public string pesan_ditolak { get; set; }
+        public string pesan_diterima { get; set; }
+
+        public CovidConfig readJson()
+        {
+            String jsonString = File.ReadAllText("D:\\Dokumen Kuliah\\Semester 4\\Konstruksi Perangkat Lunak Praktikum\\TP MODUL 8\\TpModul8\\covid_config.json");
+            var options = new JsonSerializerOptions
+            {
+                IncludeFields = true
+            };
+            CovidConfig json = JsonSerializer.Deserialize<CovidConfig>(jsonString, options);
+            return json;
+        }
+        public void ubahSatuan(CovidConfig json)
+        {
+            if (json.satuan_suhu.Equals("Celcius"))
+            {
+                json.satuan_suhu = "Fahrenheit";
+            }
+            else
+            {
+                json.satuan_suhu = "Celcius";
+            }
+        }
+    }
+}
--- TpModul8/Program.cs
+++ TpModul8/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TpModul8;
+CovidConfig covid = new CovidConfig();
+CovidConfig covidConfig = covid.readJson();
+
+Console.WriteLine("(Config Awal)");
+Console.WriteLine("Satuan Suhu     : " + covidConfig.satuan_suhu);
+Console.WriteLine("Batas Hari Demam: " + covidConfig.batas_hari_demam);
+Console.WriteLine("Pesan Ditolak   : " + covidConfig.pesan_ditolak);
+Console.WriteLine("Pesan Diterima  : " + covidConfig.pesan_diterima);
+
+covidConfig.ubahSatuan(covidConfig);
+
+Console.WriteLine("(Config Setelah Ubah Satuan)");
+Console.WriteLine("Satuan Suhu     : " + covidConfig.satuan_suhu);
+Console.WriteLine("Batas Hari Demam: " + covidConfig.batas_hari_demam);
+Console.WriteLine("Pesan Ditolak   : " + covidConfig.pesan_ditolak);
+Console.WriteLine("Pesan Diterima  : " + covidConfig.pesan_diterima);
+
+Console.WriteLine("Berapa suhu badan anda saat ini ? Dalam nilai " + covidConfig.satuan_suhu);
+double suhu = double.Parse(Console.ReadLine());
+bool cekSuhu, cekLama;
+if (covidConfig.satuan_suhu == "Celcius")
+{
+    cekSuhu = suhu > 36.5 && suhu < 37.5;
+}
+else if (covidConfig.satuan_suhu == "Fahrenheit")
+{
+    cekSuhu = suhu > 97.7 && suhu < 99.5;
+}
+else
+{
+    cekSuhu = false;
+}
+Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala deman? ");
+int lama = int.Parse(Console.ReadLine());
+if (lama < covidConfig.batas_hari_demam)
+{
+    cekLama = true;
+}
+else
+{
+    cekLama = false;
+}
+Console.WriteLine(cekLama + " " + cekSuhu);
+if (cekSuhu == true && cekLama == true)
+{
+    Console.WriteLine("Output : " + covidConfig.pesan_diterima);
+}
+else
+{
+    Console.WriteLine("Output : " + covidConfig.pesan_ditolak);
+}
